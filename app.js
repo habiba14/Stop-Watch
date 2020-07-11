91d6b484b2287f1b5d0af68f79c0817cf605f1aa
habiba14 <57413522+habiba14@users.noreply.github.com>
2020-07-11 22:59:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var min = 0;
+var sec = 0;
+var ms = 0;
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msHeading = document.getElementById("ms");
+var interval;
+
+function timer(){
+ms++
+msHeading.innerHTML = ms;
+
+if (ms >= 100){
+    sec++
+    secHeading.innerHTML = sec;
+     ms = 0;
+}
+
+else if (sec >=60) {
+min++
+     sec = 0;
+     minHeading.innerHTML = min
+
+ }
+}
+
+ function start(){
+    interval = setInterval(timer,10)
+    document.getElementById("strdis").disabled = true;
+        document.getElementById("strdis").disabled = false;
+
+ }
+
+function stop(){
+    clearInterval(interval)
+    document.getElementById("strdis").disabled = false;
+
+}
+
+function reset(){
+    min = 0;
+    sec = 0;
+    ms = 0;
+    minHeading.innerHTML = min
+    secHeading.innerHTML = sec
+    msHeading.innerHTML = ms
+    stop()
+    document.getElementById("strdis").disabled = false;
+
+}
